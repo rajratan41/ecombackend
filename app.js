5f9332e58941a55cc819126a8892aa479e957f41
Raj Ratan <raj808341@gmail.com>
2023-02-16 20:40:17 +0530

    updating signup route

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
+// regular middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // morgan middleware
 app.use(morgan("tiny"));
-app.get("/", (req, res) => {
-  res.send("Home");
-});
+
+// import all routes
+const home = require("./routes/home");
+const user = require("./routes/user");
+// router middleware
+app.use("/api/v1", user);
+app.use("/api/v1", home);
+
 module.exports = app;
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router;
+const router = express.Router();
 const { signup } = require("../controllers/userController");
 
 router.route("/signup").post(signup);
