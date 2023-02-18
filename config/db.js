c945ff68408d2ffe564ddf71931d3808b829299a
Raj Ratan <raj808341@gmail.com>
2023-02-18 20:18:36 +0530

    minor fix

--- config/db.js
+++ config/db.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
 
-const connectToDB = mongoose
-  .connect(process.env.MONGO_URI)
-  .then((conn) => {
-    console.log(`Connected to DB ${conn.connection.host}`);
-  })
-  .catch((err) => {
-    console.log(err.message);
-    process.exit(1);
-  });
+const connectToDB = () => {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then((conn) => {
+      console.log(`Connected to DB ${conn.connection.host}`);
+    })
+    .catch((err) => {
+      console.log(err.message);
+      process.exit(1);
+    });
+};
 
 module.exports = connectToDB;
