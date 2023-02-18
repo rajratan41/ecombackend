48e4e2e0e70cce74e13b455cdb02feb53c64524a
Raj Ratan <raj808341@gmail.com>
2023-02-18 20:10:01 +0530

    connecting to DB

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const app = require("./app");
 const PORT = process.env.PORT;
+const connectToDB = require("./config/db");
+
+// connecting to Database
+connectToDB();
 
 app.listen(PORT, () => {
   console.log(`App is running at http://localhost:${PORT}`);
