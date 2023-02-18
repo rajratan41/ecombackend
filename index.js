d9a3211bb9734e818c06070b7de0e08d36f4039b
Raj Ratan <raj808341@gmail.com>
2023-02-18 20:34:37 +0530

    importing cloudinary- for image upload

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const app = require("./app");
 const PORT = process.env.PORT;
 const connectToDB = require("./config/db");
 
+// importing cloudinary to upload image
+const cloudinary = require("cloudinary");
+
 // connecting to Database
 connectToDB();
+
+// Cloudinary config goes here
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 app.listen(PORT, () => {
   console.log(`App is running at http://localhost:${PORT}/api/v1`);
